058e782553f61522aea0d102a221e1bca37d098e
fatmaugurlu <fatmaugurlu89@hotmail.com>
2021-01-25 00:03:03 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] number;
+        V[] name;
+        K[] tempNumber;
+        V[] tempName;
+
+        public MyDictionary()         //constructor
+        {
+            number = new K[0];
+            name = new V[0];
+        }
+
+        public void Add(K items1, V items2)
+        {
+            tempNumber = number;
+            tempName = name;
+            number = new K[number.Length + 1];
+            name = new V[name.Length + 1];
+
+            for (int i = 0; i < tempNumber.Length; i++)
+            {
+                number[i] = tempNumber[i];
+            }
+
+            number[number.Length - 1] = items1;
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+
+            name[name.Length - 1] = items2;
+        }
+
+        public K[] items1
+        {
+            get { return number; }
+        }
+        public V[] items2
+        {
+            get { return name; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(25, "Fatma Uğurlu");
+            students.Add(32, "Emre Pelvan");
+            students.Add(48, "Ayşe Bedel");
+            students.Add(76, "Gözde Şimşek");
+            students.Add(102, "Elif Kuzoluk");
+
+            Console.WriteLine("Öğrencilerin Lİstesi");
+
+            for (int i = 0; i < students.items1.Length; i++)
+            {
+                Console.WriteLine("Öğrenci Numarası: " + students.items1[i] + " " + "Öğrenci Adı: " + students.items2[i]);
+            }
+
+
+        }
+    }
+}
